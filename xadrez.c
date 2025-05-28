756c1a003f4ac8458f2fb3ac582d7316cd0c2e92
IcaroTeles <114035956+IcaroTeles@users.noreply.github.com>
2025-05-28 22:04:04 +0000

    IMPLEMENTAÇÃO DE MOVIMENTO DAS PEÇAS

--- xadrez.c
+++ xadrez.c
@@ -5,17 +5,38 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    //========== DECLARAÇÃO DE VARIÁVEIS
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    int bispo = 1, torre = 1, rainha = 1;
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    //========== Implementação de Movimentação do Bispo
+    
+    for (bispo; bispo <= 5; bispo++) {
+        printf (" Bispo Andou para a DIREITA/CIMA!\n");
+    }
+
+    printf ("==========\n");
+
+    // ========== Implementação de Movimentação da Torre
+
+    
+    while (torre <= 5) {
+        printf (" Torre andou Direita!\n");
+        torre++;
+    }
+    
+    printf ("==========\n");
+
+    //==========  Implementação de Movimentação da Rainha
+    
+    do {
+        printf ("Rainha andou para a ESQUERDA!\n");
+        rainha++;
+    } while (rainha <= 8);
+    
+    printf ("==========\n");
+    
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
